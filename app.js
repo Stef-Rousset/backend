dc194a5b4288f13491a5bb32988c877616c8adbe
stephanie rousset <strabon@club-internet.fr>
2021-08-21 15:26:51 +0200

    add controllers folder

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 // import des routes
-app.use('/api/routes', stuffRoutes);
+app.use('/api/stuff', stuffRoutes);
 
 
 module.exports = app;
